9709f46bc7808594210ebdb40f7a23089068e950
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2020-11-01 13:22:57 -0700

    Counting numbers function works

--- Lists.hs
+++ Lists.hs
@@ -1,5 +1,7 @@
 module Lists where
 
+  countingNumbers = [1..]
+
   -- I've left this sample definition in here, you should delete it
   oddNumbers :: Int -> [Int]
   oddNumbers n = [ x | x <- [1..n], (mod x 1) == 0] 
