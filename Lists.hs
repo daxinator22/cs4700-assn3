b0073e5700015927b376806bf8205048d5fa2cc3
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2020-11-01 14:04:17 -0700

    Merge function now works

--- Lists.hs
+++ Lists.hs
@@ -2,8 +2,8 @@ module Lists where
 
   countingNumbers = [1..]
   evenNumbers = [ x | x <- [1..], (mod x 2) == 0]
-
-
+  isPrime k = [x | x <- [2..k - 1], k `mod` x == 0] 
+  merge x y = x ++ y
   slice range x = take ((snd range) - (fst range) + 1) (drop ((fst range) - 1) x)
 
   -- I've left this sample definition in here, you should delete it
