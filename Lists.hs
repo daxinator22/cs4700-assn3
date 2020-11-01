acde5ebcb31191cbd996b3ab7029a52ae43125e9
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2020-11-01 13:42:52 -0700

    Slice function now works

--- Lists.hs
+++ Lists.hs
@@ -1,6 +1,10 @@
 module Lists where
 
   countingNumbers = [1..]
+  evenNumbers = [ x | x <- [1..], (mod x 2) == 0]
+
+
+  slice range x = take ((snd range) - (fst range) + 1) (drop ((fst range) - 1) x)
 
   -- I've left this sample definition in here, you should delete it
   oddNumbers :: Int -> [Int]
